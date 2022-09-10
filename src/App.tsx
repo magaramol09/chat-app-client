008144c2b60d0a9c67f23412539c149b4f635c8f
amol-magar <magaramol09@gmail.com>
2022-09-11 00:24:31 +0530

    added tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,3 @@
-import './App.css'
-
-function App() {
-
-  return (
-    <div className="App">
-     <p>hello world</p>
-    </div>
-  )
+export default function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
-  
-export default App
